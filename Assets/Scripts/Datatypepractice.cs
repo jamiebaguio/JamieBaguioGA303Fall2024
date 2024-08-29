3fb600fc4ba8181d00bca2e269b816e00a638183
jamiebaguio <jamiebaguio@lcad.edu>
2024-08-29 12:32:02 -0700

    Update Datatypepractice.cs

--- Assets/Scripts/Datatypepractice.cs
+++ Assets/Scripts/Datatypepractice.cs
@@ -25,11 +25,25 @@ public class Datatyepractice : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Starting total is " + additionTotal);
-        additionTotal = additionTotal + num1;
+        Debug.Log("Starting number for num1 is: " + num1);
+        num1 = num1 + 1;
+        Debug.Log("The end number for num1 after adding 1 is: " + num1);
 
-        Debug.Log("added num1 to additionTotal. Total is now " + additionTotal);
+        Debug.Log("Starting number for num2 is: " - num2);
+        num2 = num2 - 1;
+        Debug.Log("The end number for num2 subtracting 1 is: " - num1);
+
+        Debug.Log("Starting number for num5 is: " * num5);
+        num5 = num5 * 2;
+        Debug.Log("The end number for num5 multiply 2 is: " * num10)
+
+        Debug.Log("Starting number for num6 is: " / num3);
+        num6 = num6 / 3;
+        Debug.Log("The end number for num6 is: " / 2)
+
+        
     }
+}
 
     // Update is called once per frame
     void Father()
