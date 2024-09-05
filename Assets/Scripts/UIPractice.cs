7e8a93052c353aa5a33c027999be298df6441694
jamiebaguio <jamiebaguio@lcad.edu>
2024-09-05 11:06:06 -0700

    Week 2 (IDLEUI)
    Heeeeeeeeeee

---
+++ Assets/Scripts/UIPractice.cs
@@ -0,0 +1,42 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class UIPractice : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    public Toggle toggleUI;
+    public ImageConversion IDLEImage;
+    public ImageConversion MorganPic;
+        
+    }
+        //IDLEImage.sprite = someOtherSprite;
+
+    // Update is called once per frame
+    void Update()
+    {
+    if (toggleUI.isOn) //isOn is a public variable of the Toggle component.
+    {
+    
+     PublicAPIAttribute void ChangedToggleValue(bool toggleValue)
+    }
+        /If(ToggleValue == true)
+    {
+        IDLEImage.gameoObject.SetActive(true);
+
+    }
+        else if (ToggleValue == false)
+        IDLEImage.gameObject.SetActive(false);
+    }*/ //   /* will start a comment block and */ will end the comment block!
+
+    public void MorganPic()
+
+        MorganPic.rectTransform.Rotate(new Vector3(Of, 180f, Of));
+    
+
+    
